4a909388b3cfd8042794efb125bb6d42a6bcc2d1
Scott Donaldson <scott@isl.co>
2018-10-01 16:03:17 -0400

    fixes to distance functions

--- utils/manhattanDistance.js
+++ utils/manhattanDistance.js
@@ -2,39 +2,32 @@ import { Agent } from '../agents/Agent';
 
 /**
  * Finds the Manhattan distance between `p1` and `p2`.
- * Expects that p1 and p2 each contain `x`, `y`, and `z`
- * keys that have numeric values, or are Agents with `x`, `y`, and `z` values.
+ * The inputs may be plain objects
+ * with `x`, `y`, and/or `z` keys, or Agent-like objects who have
+ * `x`, `y`, and/or `z` data.
  * @param {*} p1 
  * @param {*} p2 
  * @return {number} The Manhattan distance between p1 and p2.
  */
 export default function manhattanDistance(p1, p2) {
 
-    const a = {};
-    const b = {};
+    let x1 = (p1 instanceof Agent ? p1.get('x') : p1.x) || 0;
+    let y1 = (p1 instanceof Agent ? p1.get('y') : p1.y) || 0;
+    let z1 = (p1 instanceof Agent ? p1.get('z') : p1.z) || 0;
 
-    if (p1 instanceof Agent) {
-        a.x = p1.get('x');
-        a.y = p1.get('y');
-        a.z = p1.get('z');
+    let x2 = (p2 instanceof Agent ? p2.get('x') : p2.x) || 0;
+    let y2 = (p2 instanceof Agent ? p2.get('y') : p2.y) || 0;
+    let z2 = (p2 instanceof Agent ? p2.get('z') : p2.z) || 0;
+
+    let dx = Math.abs(x2 - x1);
+    let dy = Math.abs(y2 - y1);
+    let dz = Math.abs(z2 - z1);
+    
+    if (p1.environment && p1.environment.width && p1.environment.height) {
+        const { width, height } = p1.environment;
+        if (dx > width / 2) dx = width - dx;
+        if (dy > height / 2) dy = height - dy;
     }
-
-    if (p2 instanceof Agent) {
-        b.x = p2.get('x');
-        b.y = p2.get('y');
-        b.z = p2.get('z');
-    }
-
-    if (!a.x) a.x = 0;
-    if (!a.y) a.y = 0;
-    if (!a.z) a.z = 0;
-    if (!b.x) b.x = 0;
-    if (!b.y) b.y = 0;
-    if (!b.z) b.z = 0;
-
-    const dx = Math.abs(b.x - a.x);
-    const dy = Math.abs(b.y - a.y);
-    const dz = Math.abs(b.z - a.z);
     
     return dx + dy + dz;
 };
