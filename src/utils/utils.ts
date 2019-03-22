8ae1248ec91d68c57ec24f1f83f9d53c1cf9eafd
Scott Donaldson <scott.p.donaldson@gmail.com>
2019-03-22 16:29:20 -0400

    add uuid util function

--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -1,13 +1,14 @@
-import clamp from './clamp';
-import distance from './distance';
-import gaussian from './gaussian';
-import manhattanDistance from './manhattanDistance';
-import remap from './remap';
-import sample from './sample';
-import shuffle from './shuffle';
-import sum from './sum';
-import mean from './mean';
-import stdDev from './stdDev';
+import clamp from "./clamp";
+import distance from "./distance";
+import gaussian from "./gaussian";
+import manhattanDistance from "./manhattanDistance";
+import remap from "./remap";
+import sample from "./sample";
+import shuffle from "./shuffle";
+import sum from "./sum";
+import mean from "./mean";
+import stdDev from "./stdDev";
+import uuid from "./uuid";
 
 const utils = {
   clamp,
@@ -19,7 +20,8 @@ const utils = {
   shuffle,
   sum,
   mean,
-  stdDev
+  stdDev,
+  uuid
 };
 
 export { utils };
---
+++ src/utils/uuid.ts
@@ -0,0 +1,43 @@
+function uuid(): Function {
+  // http://stackoverflow.com/questions/105034/how-to-create-a-guid-uuid-in-javascript/21963136#21963136
+
+  const lut: Array<string> = [];
+
+  for (let i = 0; i < 256; i++) {
+    lut[i] = (i < 16 ? "0" : "") + i.toString(16);
+  }
+
+  return function generate(): string {
+    const d0: number = (Math.random() * 0xffffffff) | 0;
+    const d1: number = (Math.random() * 0xffffffff) | 0;
+    const d2: number = (Math.random() * 0xffffffff) | 0;
+    const d3: number = (Math.random() * 0xffffffff) | 0;
+
+    const uuid =
+      lut[d0 & 0xff] +
+      lut[(d0 >> 8) & 0xff] +
+      lut[(d0 >> 16) & 0xff] +
+      lut[(d0 >> 24) & 0xff] +
+      "-" +
+      lut[d1 & 0xff] +
+      lut[(d1 >> 8) & 0xff] +
+      "-" +
+      lut[((d1 >> 16) & 0x0f) | 0x40] +
+      lut[(d1 >> 24) & 0xff] +
+      "-" +
+      lut[(d2 & 0x3f) | 0x80] +
+      lut[(d2 >> 8) & 0xff] +
+      "-" +
+      lut[(d2 >> 16) & 0xff] +
+      lut[(d2 >> 24) & 0xff] +
+      lut[d3 & 0xff] +
+      lut[(d3 >> 8) & 0xff] +
+      lut[(d3 >> 16) & 0xff] +
+      lut[(d3 >> 24) & 0xff];
+
+    // .toUpperCase() here flattens concatenated strings to save heap memory space.
+    return uuid.toUpperCase();
+  };
+}
+
+export default uuid();
