cc57b034fb5f907f3953f3ff14e31c6e24bc40a9
Scott Donaldson <scott.p.donaldson@gmail.com>
2019-05-09 06:02:08 -0400

    docs for random util

--- src/utils/random.ts
+++ src/utils/random.ts
@@ -1,4 +1,11 @@
-export default function random(
+/**
+ * Return a random integer (or float)
+ * between `min` and `max`
+ * @param min
+ * @param max
+ * @param {boolean} float - If true, returns a float. If false or empty, returns an int.
+ */
+function random(
   min: number = 0,
   max: number = 1,
   float: boolean = false
@@ -7,3 +14,5 @@ export default function random(
   if (!float) r = Math.round(r);
   return min + r;
 }
+
+export default random;
