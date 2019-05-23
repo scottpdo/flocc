2a50d0673d54fcf741e25a17491291e81ea32c94
Scott Donaldson <scott@isl.co>
2019-05-23 16:37:02 -0400

    interface for adding metric to line chart and rendering

--- src/renderers/LineChartRenderer.ts
+++ src/renderers/LineChartRenderer.ts
@@ -1,18 +1,34 @@
 /// <reference path="./Renderer.d.ts" />
+/// <reference path="../types/Point.d.ts" />
 import { Environment } from "../environments/Environment";
 import { Agent } from "../agents/Agent";
+
+declare interface Metric {
+  color: string;
+  buffer: HTMLCanvasElement;
+  location: Point;
+  key: string;
+}
+
+interface MetricOptions {
+  color: string;
+}
+
+const defaultOptions: MetricOptions = {
+  color: "#000"
+};
 
 class LineChartRenderer implements Renderer {
   /** @member Environment */
   environment: Environment;
   /** @member HTMLPreElement */
   canvas: HTMLCanvasElement;
-  metrics: Object;
+  metrics: Metric[];
 
-  constructor(environment: Environment, opts = {}) {
+  constructor(environment: Environment) {
     this.canvas = document.createElement("canvas");
     this.environment = environment;
-    this.metrics = {};
+    this.metrics = [];
     environment.renderers.push(this);
   }
 
@@ -24,12 +40,50 @@ class LineChartRenderer implements Renderer {
     }
   }
 
-  metric(key: string, opts = {}) {
-    console.log("setting linechart metric", key);
-    this.metrics[key] = {};
+  metric(key: string, opts: MetricOptions = defaultOptions) {
+    const buffer = document.createElement("canvas");
+    buffer.width = this.canvas.width;
+    buffer.height = this.canvas.height;
+    this.metrics.push({
+      opts.color,
+      key,
+      buffer,
+      location: { x: -1, y: -1 }
+    });
   }
 
-  render() {}
+  render() {
+    const agents = this.environment.getAgents();
+    const { time } = this.environment;
+    const means: Map<string, number> = new Map();
+    agents.forEach(agent => {
+      this.metrics.forEach(metric => {
+        const { key } = metric;
+        const data = agent.get(key);
+        if (isNaN(data)) return;
+        means.set(key, !means.get(key) ? data : means.get(key) + data);
+      });
+    });
+    this.canvas
+      .getContext("2d")
+      .clearRect(0, 0, this.canvas.width, this.canvas.height);
+    this.metrics.forEach(metric => {
+      const { buffer, color, location, key } = metric;
+      let { x, y } = location;
+      const context = buffer.getContext("2d");
+      const mean = means.get(key) / agents.length;
+
+      context.strokeStyle = color;
+      if (x < 0) y = this.canvas.height - mean;
+      context.moveTo(x, y);
+      context.lineTo(x + 1, this.canvas.height - mean);
+      context.stroke();
+      location.x++;
+      location.y = this.canvas.height - mean;
+
+      this.canvas.getContext("2d").drawImage(buffer, 0, 0);
+    });
+  }
 }
 
 export { LineChartRenderer };
