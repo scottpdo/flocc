37c918139b2f7303d3ed0dc990a986a90074e556
Scott Donaldson <scott.p.donaldson@gmail.com>
2019-06-11 08:07:32 -0400

    more LineChartRenderer tests

--- tests/linechart.test.js
+++ tests/linechart.test.js
@@ -1,5 +1,11 @@
-const { Agent, Environment, LineChartRenderer } = require("../dist/flocc");
+const {
+  Agent,
+  Environment,
+  LineChartRenderer,
+  utils
+} = require("../dist/flocc");
 
+const color = "#ff0000";
 const width = 200;
 const height = 400;
 
@@ -12,7 +18,19 @@ const renderer = new LineChartRenderer(environment, {
 it("Correctly instantiates a LineChartRenderer", () => {
   expect(renderer.canvas).toBeInstanceOf(HTMLCanvasElement);
   expect(renderer.background).toBeInstanceOf(HTMLCanvasElement);
+
   expect(renderer.environment).toBe(environment);
+  expect(environment.renderers).toContain(renderer);
+
   expect(renderer.width).toEqual(width);
   expect(renderer.height).toEqual(height);
+});
+
+it("Can add metrics to a LineChartRenderer", () => {
+  renderer.metric("x");
+  expect(renderer.metrics).toHaveLength(1);
+  renderer.metric("x", { color: "#f00" });
+  expect(renderer.metrics).toHaveLength(2);
+  renderer.metric("y", { fn: utils.sum });
+  expect(renderer.metrics).toHaveLength(3);
 });
