b937ac929e106fa66fe3c8653fc419513dff815d
Scott Donaldson <scott@isl.co>
2019-10-07 15:28:11 -0400

    template client-side testing w/ express

---
+++ client/app.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const fs = require("fs");
+const app = express();
+const port = 3000;
+
+app.use('/dist', express.static(__dirname + "/dist"));
+
+app.get("*", (req, res) => {
+  const header = fs.readFileSync(__dirname + "/partials/header.html");
+  const footer = fs.readFileSync(__dirname + "/partials/footer.html");
+  let path = req.path;
+  if (path === "/") path = "/index";
+  if (fs.existsSync(__dirname + "/pages" + path + ".html")) {
+    res.send(
+      header + fs.readFileSync(__dirname + "/pages" + path + ".html") + footer
+    );
+  } else {
+    res.send(header + "404" + footer);
+  }
+});
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`));
--- rollup.config.js
+++ rollup.config.js
@@ -1,22 +1,27 @@
-import typescript from 'rollup-plugin-typescript2';
+import typescript from "rollup-plugin-typescript2";
 
 export default {
-  input: 'src/main.ts',
+  input: "src/main.ts",
   output: [
     {
-      file: 'dist/flocc.js',
-      format: 'umd',
-      name: 'flocc'
+      file: "dist/flocc.js",
+      format: "umd",
+      name: "flocc"
     },
     {
-      file: 'dist/flocc.es.js',
-      format: 'es',
-      name: 'flocc'
+      file: "dist/flocc.es.js",
+      format: "es",
+      name: "flocc"
+    },
+    {
+      file: "client/dist/flocc.js",
+      format: "umd",
+      name: "flocc"
     }
   ],
   plugins: [
     typescript({
-      typescript: require('typescript')
+      typescript: require("typescript")
     })
   ]
 };
