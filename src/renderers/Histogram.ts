e4157547c198748a7280295a747e488fe7fc564a
Scott Donaldson <scott@isl.co>
2019-10-24 16:58:36 -0400

    extractRoundNumbers abstracted, histogram closer

--- src/renderers/Histogram.ts
+++ src/renderers/Histogram.ts
@@ -3,21 +3,17 @@
 /// <reference path="../types/NRange.d.ts" />
 import { Environment } from "../environments/Environment";
 import remap from "../utils/remap";
-import max from "../utils/max";
-
-interface MetricOptions extends NRange {
-  color: string;
-}
-
-interface Metric extends MetricOptions {
-  key: string;
-}
+import { default as getMax } from "../utils/max";
+import extractRoundNumbers from "../utils/extractRoundNumbers";
 
 interface HistogramOptions {
   aboveMax: boolean;
   belowMin: boolean;
   buckets: number;
+  color: string;
   height: number;
+  max: number;
+  min: number;
   width: number;
   scale: "relative" | "fixed";
 }
@@ -26,15 +22,12 @@ const defaultHistogramOptions: HistogramOptions = {
   aboveMax: false,
   belowMin: false,
   buckets: 1,
+  color: "#000",
   height: 500,
+  max: 1,
+  min: 0,
   width: 500,
   scale: "fixed"
-};
-
-const defaultMetricOptions: MetricOptions = {
-  color: "#000",
-  min: 0,
-  max: 1
 };
 
 class Histogram implements Renderer {
@@ -43,28 +36,30 @@ class Histogram implements Renderer {
   /** @member HTMLCanvasElement */
   canvas: HTMLCanvasElement = document.createElement("canvas");
   background: HTMLCanvasElement = document.createElement("canvas");
-  metrics: Metric[] = [];
+  _metric: string;
   opts: HistogramOptions = defaultHistogramOptions;
   height: number;
+  markerWidth: number = 0;
   width: number;
 
   constructor(environment: Environment, opts?: HistogramOptions) {
     this.environment = environment;
     this.opts = Object.assign(this.opts, opts);
     const { width, height } = this.opts;
-    this.width = width;
-    this.height = height;
-    this.canvas.width = width;
-    this.canvas.height = height;
+    const dpr = window.devicePixelRatio;
+    this.width = width * dpr;
+    this.height = height * dpr;
+    this.canvas.width = width * dpr;
+    this.canvas.height = height * dpr;
+    this.canvas.style.width = width + "px";
+    this.canvas.style.height = height + "px";
+    this.background.width = width * dpr;
+    this.background.height = height * dpr;
     environment.renderers.push(this);
   }
 
-  metric(key: string, opts?: MetricOptions) {
-    this.metrics.push(
-      Object.assign({}, defaultMetricOptions, opts, {
-        key
-      })
-    );
+  metric(_metric: string): void {
+    this._metric = _metric;
   }
 
   mount(el: string | HTMLElement): void {
@@ -75,56 +70,103 @@ class Histogram implements Renderer {
     }
   }
 
+  x(value: number): number {
+    const { width, markerWidth } = this;
+    return remap(value, 0, width, markerWidth, width);
+  }
+
+  y(value: number, maxValue: number): number {
+    const { height } = this;
+    return remap(value, 0, maxValue, height - 30, 0);
+  }
+
+  drawMarkers(bucketValues: Array<number>): void {
+    const context = this.canvas.getContext("2d");
+    const { environment, height, width } = this;
+    const metric = this._metric;
+    const agents = environment.getAgents();
+    const { buckets, scale, min, max } = this.opts;
+    const yMin = 0;
+    const yMax = scale === "fixed" ? agents.length : getMax(bucketValues);
+    const markers = extractRoundNumbers({ min: yMin, max: yMax });
+    context.fillStyle = "black";
+    context.font = `${14 * window.devicePixelRatio}px Helvetica`;
+    markers.forEach(marker => {
+      const { width } = context.measureText(marker.toLocaleString());
+      if (width > this.markerWidth) this.markerWidth = width;
+      context.textAlign = "left";
+      context.textBaseline = "middle";
+      context.fillText(marker.toLocaleString(), 0, this.y(marker, yMax));
+    });
+
+    // draw lines
+    markers.forEach(marker => {
+      context.moveTo(this.markerWidth + 10, this.y(marker, yMax));
+      context.lineTo(this.width, this.y(marker, yMax));
+      context.setLineDash([10, 10]);
+      context.stroke();
+    });
+
+    bucketValues
+      .map((v, i) => {
+        return (
+          remap(i, 0, bucketValues.length, min, max).toLocaleString() +
+          "–" +
+          remap(i + 1, 0, bucketValues.length, min, max).toLocaleString()
+        );
+      })
+      .forEach((label, i) => {
+        context.textAlign = "center";
+        context.textBaseline = "alphabetic";
+        context.fillText(
+          label,
+          this.x((i * width) / buckets + (0.5 * width) / buckets),
+          height - 3
+        );
+      });
+  }
+
   render(): void {
-    const { canvas, environment, metrics } = this;
-    const { buckets, scale } = this.opts;
-    const width = 500;
-    const height = 200;
+    if (!this._metric) return;
+    const { canvas, environment, width, height } = this;
+    const metric = this._metric;
+    const { buckets, color, scale, max, min } = this.opts;
     const context = canvas.getContext("2d");
 
     const agents = environment.getAgents();
 
     context.clearRect(0, 0, width, height);
 
-    // initialize map of bucket values -- for each metric, a pairing of `key` and an
+    // initialize map of bucket values --
     // array of length `buckets`, initialized to all zeros
-    const mapOfBucketValues: Map<string, Array<number>> = new Map();
-    metrics.forEach(({ key }) =>
-      mapOfBucketValues.set(key, new Array(buckets).fill(0))
-    );
+    const bucketValues = new Array(buckets).fill(0);
 
     agents.forEach(agent => {
-      metrics.forEach(({ key }) => {
-        // ignore this agent if it doesn't have the metric key in question
-        if (agent.get(key) === null) return;
+      // ignore this agent if it doesn't have the metric in question
+      if (agent.get(metric) === null) return;
 
-        // get reference to array of bucket values
-        const bucketValues = mapOfBucketValues.get(key);
+      // calculate index of bucket this agent's value says it belongs in
+      const bucketIndex = Math.floor(
+        remap(agent.get(metric), min, max, 0, 0.999999) * buckets
+      );
 
-        // calculate index of bucket this agent's value says it belongs in
-        const bucketIndex = Math.floor(
-          remap(agent.get(key), min, max, 0, 0.999999) * buckets
-        );
-
-        // increment the corresponding value in the bucketValues array
-        bucketValues[bucketIndex]++;
-      });
+      // increment the corresponding value in the bucketValues array
+      bucketValues[bucketIndex]++;
     });
 
-    metrics.forEach(({ color, key }) => {
-      context.fillStyle = color;
+    this.drawMarkers(bucketValues);
 
-      const bucketValues = mapOfBucketValues.get(key);
-      const maxValue = scale === "fixed" ? agents.length : max(bucketValues);
-      bucketValues.forEach((value, i) => {
-        const mappedValue = remap(value, 0, maxValue, 0, 1);
-        context.fillRect(
-          i * (width / buckets),
-          remap(mappedValue, 0, 1, height, 0),
-          width / buckets,
-          remap(mappedValue, 0, 1, 0, height)
-        );
-      });
+    context.fillStyle = color;
+
+    const maxValue = scale === "fixed" ? agents.length : getMax(bucketValues);
+    bucketValues.forEach((value, i) => {
+      const mappedValue = remap(value, 0, maxValue, 0, 1);
+      context.fillRect(
+        this.x(((i + 0.1) * width) / buckets),
+        remap(mappedValue, 0, 1, height - 30, 0),
+        (0.8 * (width - this.markerWidth)) / buckets,
+        remap(mappedValue, 0, 1, 0, height - 30)
+      );
     });
   }
 }
