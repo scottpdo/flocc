03b047cddf59bfdae1a5cfe493ee69ab1395381e
Scott Donaldson <scott@isl.co>
2019-12-12 11:04:26 -0500

    adding KDTree tests

--- tests/kdtree.test.js
+++ tests/kdtree.test.js
@@ -1,18 +1,30 @@
 const { Agent, Environment, KDTree, utils } = require("../dist/flocc");
 
 const environment = new Environment();
-for (let i = 0; i < 100; i++) {
-  const agent = new Agent();
-  agent.set({
-    x: utils.random(-1000, 1000),
-    y: utils.random(-1000, 1000),
-    z: utils.random(-1000, 1000)
-  });
-  environment.addAgent(agent);
+
+for (let x = 0; x < 25; x++) {
+  for (let y = 0; y < 25; y++) {
+    for (let z = 0; z < 25; z++) {
+      const agent = new Agent();
+      agent.set({ x, y, z });
+      environment.addAgent(agent);
+    }
+  }
 }
 
 const tree = new KDTree(environment.getAgents(), 3);
+environment.use(tree);
+
+it("Has the same agents as environment at top level.", () => {
+  expect(tree.agents).toBe(environment.getAgents());
+});
+
 it("Creates a median", () => {
-  // console.log(tree);
-  // expect(tree.median).toBe(0);
+  expect(tree.median).toBeGreaterThanOrEqual(0);
+  expect(tree.median).toBeLessThanOrEqual(25);
+});
+
+it("Has two child subtrees", () => {
+  expect(tree.left).toBeInstanceOf(KDTree);
+  expect(tree.right).toBeInstanceOf(KDTree);
 });
