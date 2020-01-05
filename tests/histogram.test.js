57cdf7498bab1bbb4305228d0543e553d1c1aae5
Scott Donaldson <sd@scottland.cc>
2020-01-05 06:05:59 -0500

    histogram tests

--- tests/histogram.test.js
+++ tests/histogram.test.js
@@ -15,6 +15,14 @@ beforeEach(() => {
   });
   histogram.metric("x");
   histogram.mount(container);
+  histogram.canvas.getContext("2d").__clearEvents();
+  histogram.canvas.getContext("2d").__clearDrawCalls();
+});
+
+afterEach(() => {
+  while (environment.getAgents().length > 0) {
+    environment.getAgents().pop();
+  }
 });
 
 it("Correctly instantiates a histogram", () => {
@@ -29,6 +37,30 @@ it("Correctly instantiates a histogram", () => {
 });
 
 it("Correctly renders", () => {
+  environment.tick();
+  const calls = histogram.canvas.getContext("2d").__getDrawCalls();
+  expect(calls).toMatchSnapshot();
+});
+
+it("Correctly renders with agents", () => {
+  for (let i = 0; i < 100; i++) {
+    const agent = new Agent();
+    agent.set("x", Math.sqrt(i));
+    environment.addAgent(agent);
+  }
+  environment.tick();
+  const calls = histogram.canvas.getContext("2d").__getDrawCalls();
+  expect(calls).toMatchSnapshot();
+});
+
+it("Correctly renders with discrete buckets", () => {
+  histogram.opts.background = "yellow";
+  histogram.opts.buckets = [1, 2, 3];
+  for (let i = 0; i < 10; i++) {
+    const agent = new Agent();
+    agent.set("x", i % 4);
+    environment.addAgent(agent);
+  }
   environment.tick();
   const calls = histogram.canvas.getContext("2d").__getDrawCalls();
   expect(calls).toMatchSnapshot();
