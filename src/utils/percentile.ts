3194b2d763abe985997024292ac88dd29f4c295d
Scott Donaldson <sd@scottland.cc>
2020-01-30 08:27:56 -0500

    don't loop over arr again in percentile

--- src/utils/percentile.ts
+++ src/utils/percentile.ts
@@ -1,35 +1,27 @@
 import copyArray from "./copyArray";
 import min from "./min";
 import max from "./max";
-import median from "./median";
 import lerp from "./lerp";
 
 export default function percentile(arr: number[], n: number): number {
   if (arr.length === 0) return null;
   if (arr.length === 1) return arr[0];
   if (n === 0) return min(arr);
-  if (n === 0.5) return median(arr);
   if (n === 1) return max(arr);
 
   const copy = copyArray(arr);
-  const orig = n;
+  const len = copy.length - 1;
+  copy.sort((a, b) => (a < b ? -1 : 1));
 
-  if (n <= 0.5) {
-    copy.sort((a, b) => (a < b ? -1 : 1));
-  } else {
-    copy.sort((a, b) => (a < b ? 1 : -1));
-    n = 1 - n;
-  }
+  const nl = n * len;
+  const i = nl | 0;
 
-  for (let i = 0; i < copy.length; i++) {
-    const p = i / (copy.length - 1);
-    // if at percentile n/p, return this element
-    if (n === p) return copy[i];
-    // if p has overshot n, linearly interpolate between elements
-    if (n < p) {
-      const interp = n * (copy.length - 1) - (i - 1);
-      console.log("interpolating", copy[i - 1], copy[i], interp);
-      return lerp(copy[i - 1], copy[i], interp);
-    }
-  }
+  // if n is close enough to the index of an element in the array
+  // (i.e. 90th percentile with 100 elements), return that element
+  const isEven = Math.abs(nl - i) <= 0.001;
+  if (isEven) return copy[i];
+
+  // if n falls between element indices, then interpolate
+  const interp = nl - i;
+  return lerp(copy[i], copy[i + 1], interp);
 }
