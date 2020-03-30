2ebb8d585c0b68c659a9cbae5d572aff93aee570
Scott Donaldson <sd@scottland.cc>
2020-03-30 14:42:02 -0400

    cleanup

--- client/app.js
+++ client/app.js
@@ -9,20 +9,6 @@ app.use("/static", express.static(__dirname + "/static"));
 app.set("view engine", "ejs");
 app.set("views", "./client/pages");
 
-// app.get("*", (req, res) => {
-//   const header = fs.readFileSync(__dirname + "/partials/header.html");
-//   const footer = fs.readFileSync(__dirname + "/partials/footer.html");
-//   let path = req.path;
-//   if (path === "/") path = "/index";
-//   if (fs.existsSync(__dirname + "/pages" + path + ".html")) {
-//     res.send(
-//       header + fs.readFileSync(__dirname + "/pages" + path + ".html") + footer
-//     );
-//   } else {
-//     res.send(header + "404" + footer);
-//   }
-// });
-
 const models = fs
   .readdirSync(__dirname + "/models")
   .map(filename => filename.replace(".ejs", ""));
